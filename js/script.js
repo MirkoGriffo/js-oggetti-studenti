021b14fa4f90ce903704a5655aecb8428d84c5a6
MirkoGriffo <mirkogriffo@yahoo.it>
2021-04-13 15:25:01 +0200

    Esercizio 2

--- js/script.js
+++ js/script.js
@@ -14,3 +14,33 @@ for (var key in studente) {
     console.log(studente[key]);
 }
 
+/*
+2 - Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e 
+stampare per ognuno di essi, nome e cognome.
+*/
+
+var studenti = [
+    {
+        "nome": "Mario",
+        "cognome": "Rossi",
+        "età": 35
+
+    },
+
+    {
+        "nome": "Luigi",
+        "cognome": "Verdi",
+        "età": 23
+    },
+
+    {
+        "nome": "Massimo",
+        "cognome": "Gialli",
+        "età": 27
+    }
+]
+
+for (var i = 0; i < studenti.length; i++) {
+    console.log(studenti[i].nome + " " + studenti[i].cognome);
+};
+
